ec0218da3b11a68e91cb282890b1e97328ad8bc0
Гурьянов Вячеслав <pirass1@mail.ru>
2024-12-24 22:41:35 +0300

    3

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,15 @@
 export function toggleVisibility(popover) {
-  if (popover.style.display === 'none' || popover.style.display === '') {
-    popover.style.display = 'block';
+  const styles = popover.style;
+  
+  if (styles.display === 'none' || styles.display === '') {
+    styles.display = 'block';
   } else {
-    popover.style.display = 'none';
+    styles.display = 'none';
   }
 }
 
 export function positionPopover(popover, buttonRect) {
-  popover.style.top = `${buttonRect.top - popover.offsetHeight - 5}px`;
-  popover.style.left = `${buttonRect.left}px`;
+  const styles = popover.style;
+  styles.top = `${buttonRect.top - popover.offsetHeight - 5}px`;
+  styles.left = `${buttonRect.left}px`;
 }
-
-export function setupPopover() {
-  const popover = document.getElementById('myPopover');
-  const toggleButton = document.getElementById('toggleButton');
-
-  if (!popover || !toggleButton) {
-    console.error('Popover or toggle button not found.');
-    return;
-  }
-
-  toggleVisibility(popover);
-  const buttonRect = toggleButton.getBoundingClientRect();
-  positionPopover(popover, buttonRect);
-}
-
-export default setupPopover;
