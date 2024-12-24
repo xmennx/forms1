f0a7dc4c21e2479662034f8c0c31a188f9f7ca86
Гурьянов Вячеслав <pirass1@mail.ru>
2024-12-24 22:59:42 +0300

    6

--- src/js/__tests__/popover.test.js
+++ src/js/__tests__/popover.test.js
@@ -3,14 +3,14 @@ import { toggleVisibility, positionPopover } from '../app.js';
 describe('toggleVisibility', () => {
   test('should show popover if it is hidden', () => {
     const popover = { style: { display: 'none' } };
-    
+
     toggleVisibility(popover);
     expect(popover.style.display).toBe('block');
   });
 
   test('should hide popover if it is visible', () => {
     const popover = { style: { display: 'block' } };
-    
+
     toggleVisibility(popover);
     expect(popover.style.display).toBe('none');
   });
@@ -18,12 +18,14 @@ describe('toggleVisibility', () => {
 
 test('should position popover properly relative to the anchor', () => {
   const popover = document.createElement('div');
-  popover.style = {};  // Мокаем объект стилей
+  popover.style = {}; // Мокаем объект стилей
 
-  const buttonRect = { top: 100, left: 50, width: 200, height: 40 };
+  const buttonRect = {
+    top: 100, left: 50, width: 200, height: 40,
+  };
 
   positionPopover(popover, buttonRect);
 
-  expect(popover.style.top).toBe('95px');  // Пример вывода
+  expect(popover.style.top).toBe('95px'); // Пример вывода
   expect(popover.style.left).toBe('50px'); // Пример вывода
 });
